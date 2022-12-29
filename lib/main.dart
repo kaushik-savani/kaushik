e76eb3420ddfff95d90c31a7bcce729715fc0704
kaushikbhaisavani <kaushik.savani1712@gmail.com>
2022-12-29 09:09:53 +0530

    initial commit1

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,102 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:kaushik/ViewPage.dart';
-import 'package:kaushik/dummyproducts.dart';
-import 'package:kaushik/try.dart';
 
-void main() {
-  HttpOverrides.global = MyHttpOverrides();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: demoGrid(),
-  ));
-}
-class MyHttpOverrides extends HttpOverrides {
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
   @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: ViewPage(),
+    );
   }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items:  <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset("image/img.png",height: 20,width: 20,),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+
+String numf(String n) {
+  var numArr = n.split('');
+  String revStr = "";
+  int thousands = 0;
+  for (var i = numArr.length - 1; i >= 0; i--) {
+    if (numArr[i].toString() == ".") {
+      thousands = 0;
+    } else {
+      thousands++;
+    }
+    revStr = revStr + numArr[i].toString();
+    if (thousands == 3 && i > 0) {
+      thousands = 0;
+      revStr = revStr + ',';
+    }
+  }
+  return revStr.split('').reversed.join('');
 }
